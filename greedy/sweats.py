e1b22bd171926e007fec4a0700a4a350420a6dc0
choopick_mac <choopick11@gmail.com>
2021-03-16 17:52:13 +0900

    add dfs_bfs greedy

---
+++ greedy/sweats.py
@@ -0,0 +1,23 @@
+def solution(n, lost, reserve):
+    new_lost = list(lost)
+    # 여벌 체육복을 도난맞은 경우
+    for i in lost:
+        if i in reserve:
+            reserve.remove(i)
+            new_lost.remove(i)
+
+    # 체육 수업을 들을 수 있는 학생은 1, 없는 학생은 0
+    possible = [0 if i in new_lost else 1 for i in range(1, n + 1)]
+    
+    for i in new_lost:
+        if i + 1 in reserve:
+            possible[i - 1] = 1
+            reserve.remove(i + 1)
+        elif i - 1 in reserve:
+            possible[i - 1] = 1
+            reserve.remove(i - 1)
+    
+    answer = sum(possible)
+    return answer
+
+print(solution(7,[1,2,3,4,5,6,7],[1,2,3]))
--- tester.py
+++ tester.py
@@ -1,20 +1,64 @@
-num_net = 1
 
-def DFS(start, n, computers):
-    global num_net
-    if start == n:
-        return 
+def solution(name):
+    answer = 0
+    mid_point = (ord('A') + ord('Z')) / 2
+    for i in range(len(name)):
+        right = 1
 
-    for idx in range(start, n):
-        if computers[start][idx] == 0:
-            num_net += 1
-            DFS(idx, n, computers) 
+
+        if name[i] != 'A' and ord(name[i]) < mid_point:
+            answer += (ord(name[i]) - ord('A'))
+        elif name[i] != 'A' and ord(name[i]) > mid_point:
+            answer += (ord('Z') - ord(name[i]) + 1)
         
+        if i == len(name)-1:
+            break
+        elif name[i] != 'A':
+            for j in range(i + 1, len(name)):
+                if name[j] == 'A':
+                    right += 1
+                else:
+                    break
+            if right < len(name)/2 and right != 1:
+                answer += right
+            elif right >= len(name)/2 and right != 1:
+                answer += (len(name) - right) 
+            if right == 1:
+                answer += right
+                continue
         
-            
-def solution(n, computers):
-    DFS(0, n, computers)
+    return answer
 
-    return num_net
+print(solution("BBBAAAB")) #8
+print(solution("ABABAAAAABA")) #10
+print(solution("CANAAAAANAN")) #48
+print(solution("ABAAAAABAB")) #8
+print(solution("ABABAAAAAB")) #8
+print(solution("BABAAAAB")) #7
+print(solution("AAA")) #0
+print(solution("ABAAAAAAABA")) #6
+print(solution("AAB")) #2
+print(solution("AABAAAAAAABBB")) #11
+print(solution("ZZZ")) #5
+print(solution("BBBBAAAAAB")) #10
+print(solution("BBBBAAAABA")) #12
+'''
 
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+def solution(name):
+    answer = 0
+    begin = ['A' for i in range(len(name))]
+    mid_asc = (ord('A') + ord('Z')) / 2
+    idx = 0
+    while ''.join(begin) != name:
+        if begin[idx] != name[idx] and ord(name[i]) < mid_asc:
+            answer += (ord(name[i]) - ord('A'))
+            begin[idx] = name[idx]
+        elif begin[idx] != name[idx] and ord(name[i]) > mid_asc:
+            answer += (ord('Z') - ord(name[i]) + 1)
+            begin[idx] = name[idx]
+        
+        next_idx = 
+    return answer
+
+solution('JAZ')
+'''
