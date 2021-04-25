28ce2ace4616e58e565b9792cb050a55d3a1135d
choopick_mac <choopick11@gmail.com>
2021-04-25 12:13:12 +0900

    add web study repos

--- log_of_algorithm_study/test.py
+++ log_of_algorithm_study/test.py
@@ -1,15 +1,10 @@
-def max_profit(stock_list):
-    # 주식을 산 날로부터 가장 비싸게 팔았을 때의 수익을 담을 리스트
-    max_p_list = list()
-    max_p = 0
-    for i in range(len(stock_list) - 1):
-        max_p_list.append(max(stock_list[i+1:]) - stock_list[i])
-
-    return max(max_p_list)
+def mergeAndSortLists(list1,list2):
+    new_list = list1 + list2
+    new_list = list(set(new_list))
+    return sorted(new_list)
 
 
-# 테스트
-print(max_profit([7, 1, 5, 3, 6, 4]))
-print(max_profit([7, 6, 4, 3, 1]))
-print(max_profit([11, 13, 9, 13, 20, 14, 19, 12, 19, 13]))
-print(max_profit([12, 4, 11, 18, 17, 19, 1, 19, 14, 13, 7, 15, 10, 1, 3, 6]))
+A = [1,2,3,415,2,43,2,2,20 ,21, 23]
+B = [1,2,3,412,3,5,2,10,9, 23]
+
+print(mergeAndSortLists(A,B))
